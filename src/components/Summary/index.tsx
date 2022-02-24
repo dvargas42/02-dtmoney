48899e1a7d0f4708d201af6cb9d996bb85bfb26e
dvargas42 <dvargas.eng@gmail.com>
2022-02-24 14:56:23 -0300

    feat: implement logical to transaction rendering

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -1,10 +1,34 @@
-import { Container } from "./styles";
+import { useTransaction } from "../../hooks/useTransactions";
+import { currencyConverter } from "../../utils/converter";
 
 import incomeImg from "../../assets/income.svg";
 import outcomeImg from "../../assets/outcome.svg";
 import totalImg from "../../assets/total.svg";
 
+import { Container } from "./styles";
+
 export function Summary() {
+  const { transactions } = useTransaction();
+
+  const summary = transactions.reduce(
+    (acc, transaction) => {
+      if (transaction.type === "deposit") {
+        acc.deposit += transaction.amount;
+        acc.total += transaction.amount;
+      } else {
+        acc.withdraw -= transaction.amount;
+        acc.total -= transaction.amount;
+      }
+
+      return acc;
+    },
+    {
+      deposit: 0,
+      withdraw: 0,
+      total: 0,
+    }
+  );
+
   return (
     <Container>
       <div>
@@ -12,21 +36,23 @@ export function Summary() {
           <p>Entradas</p>
           <img src={incomeImg} alt="Entradas" />
         </header>
-        <strong>R$ 1000,00</strong>
+        <strong>{currencyConverter({ money: summary.deposit })}</strong>
       </div>
       <div>
         <header>
           <p>Saídas</p>
           <img src={outcomeImg} alt="Saídas" />
         </header>
-        <strong className="whithdraw">- R$ 500,00</strong>
+        <strong className="whithdraw">
+          {currencyConverter({ money: summary.withdraw })}
+        </strong>
       </div>
       <div className="highlight-background">
         <header>
           <p>Total</p>
           <img src={totalImg} alt="Total" />
         </header>
-        <strong>R$ 500,00</strong>
+        <strong>{currencyConverter({ money: summary.total })}</strong>
       </div>
     </Container>
   );
