c3af984dca0019009e33cfe022672719ae9cdf42
dvargas42 <dvargas.eng@gmail.com>
2022-02-23 22:39:52 -0300

    feat: implement transaction request

--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -1,6 +1,27 @@
+import { useEffect, useState } from "react";
+import { api } from "../../services/api";
+import { currencyConverter, dateConverter } from "../../utils/converter";
+
 import { Container } from "./styles";
 
+type Transaction = {
+  id: number;
+  title: string;
+  amount: number;
+  type: string;
+  category: string;
+  createdAt: string;
+};
+
 export function TransactionsTable() {
+  const [transactions, setTransactions] = useState<Transaction[]>([]);
+
+  useEffect(() => {
+    api.get("transactions").then((response) => {
+      setTransactions(response.data.transactions);
+    });
+  }, []);
+
   return (
     <Container>
       <table>
@@ -14,18 +35,16 @@ export function TransactionsTable() {
         </thead>
 
         <tbody>
-          <tr>
-            <td>Desenvolvimento de website</td>
-            <td className="deposit">R$12.000</td>
-            <td>Desenvolvimento</td>
-            <td>20/02/2021</td>
-          </tr>
-          <tr>
-            <td>Desenvolvimento de website</td>
-            <td className="withdraw">- R$1.100</td>
-            <td>Casa</td>
-            <td>17/02/2021</td>
-          </tr>
+          {transactions.map((transaction) => (
+            <tr key={transaction.id}>
+              <td>{transaction.title}</td>
+              <td className={transaction.type}>
+                {currencyConverter(transaction.amount, transaction.type)}
+              </td>
+              <td>{transaction.category}</td>
+              <td>{dateConverter(new Date(transaction.createdAt))}</td>
+            </tr>
+          ))}
         </tbody>
       </table>
     </Container>
