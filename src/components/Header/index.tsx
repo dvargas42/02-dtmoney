81e96b722c8bd15dab0028456718a8ce57d85776
dvargas42 <dvargas.eng@gmail.com>
2022-02-23 22:38:36 -0300

    feat: implement prop drilling open transaction

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,19 @@
-import { Container, Content } from "./styles";
 import logoImg from "../../assets/logo.svg";
 
-export function Header() {
+import { Container, Content } from "./styles";
+
+type HeaderProps = {
+  onOpenNewTransactionModal: () => void;
+};
+
+export function Header({ onOpenNewTransactionModal }: HeaderProps) {
   return (
     <Container>
       <Content>
         <img src={logoImg} alt="logo" />
-        <button type="button">Nova transação</button>
+        <button type="button" onClick={onOpenNewTransactionModal}>
+          Nova transação
+        </button>
       </Content>
     </Container>
   );
