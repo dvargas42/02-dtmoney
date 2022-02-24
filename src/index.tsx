281d62ed9c0e4841460939fbf1e2de453d3578e2
dvargas42 <dvargas.eng@gmail.com>
2022-02-23 22:37:41 -0300

    feat: implement mock server miragejs

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,52 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { createServer, Model } from "miragejs";
+
 import { App } from "./App";
+
 import { GlobalStyle } from "./styles/global";
+
+createServer({
+  models: {
+    transactions: Model,
+  },
+  seeds(server) {
+    server.db.loadData({
+      transactions: [
+        {
+          id: 1,
+          title: "Freelance de website",
+          type: "deposit",
+          category: "Dev",
+          amount: 6000,
+          createdAt: new Date("2021-02-12 09:00:00"),
+        },
+        {
+          id: 2,
+          title: "Aluguel",
+          type: "withdraw",
+          category: "Casa",
+          amount: 1100,
+          createdAt: new Date("2021-02-15 11:00:00"),
+        },
+      ],
+    });
+  },
+
+  routes() {
+    this.namespace = "api";
+
+    this.get("/transactions", () => {
+      return this.schema.all("transactions");
+    });
+
+    this.post("/transactions", (schema, request) => {
+      const data = JSON.parse(request.requestBody);
+
+      return schema.create("transactions", data);
+    });
+  },
+});
 
 ReactDOM.render(
   <React.StrictMode>
