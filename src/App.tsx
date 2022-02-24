04929d2d34b014b116a1117ef872db21fa012e8c
dvargas42 <dvargas.eng@gmail.com>
2022-02-24 14:52:04 -0300

    feat: implement trasactions provider

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,8 @@ import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
 import { NewTransactionModal } from "./components/NewTransactionModal";
 
+import { TransactionsProvider } from "./hooks/useTransactions";
+
 Modal.setAppElement("#root");
 
 export function App() {
@@ -19,13 +21,13 @@ export function App() {
     setIsNewTransactionModalOpen(false);
   }
   return (
-    <>
+    <TransactionsProvider>
       <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
       <Dashboard />
       <NewTransactionModal
         isOpen={isNewTransactionModalOpen}
         onRequestClose={handleCloseNewTransactionModal}
       />
-    </>
+    </TransactionsProvider>
   );
 }
